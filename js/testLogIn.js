f66286bcb8579c0582ae64b499cf96a625dbf6dc
Diego <48282657+Mijayo@users.noreply.github.com>
2019-11-12 20:06:41 +0100

    testLogIn_V6

--- js/testLogIn.js
+++ js/testLogIn.js
@@ -1,3 +1,8 @@
+// localStorage.clear();
+
+// Quitamos todos los parametros de la URL
+window.history.pushState("object or string", "Title", "/" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split("?")[0]);
+
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -8,43 +13,93 @@ window.onclick = function(event) {
     }
 };
 
+
+function conversor(usuario) {
+    return `${usuario.email}&${usuario.pwd}#${usuario.usuario}`;
+}
+
 let cont = 0;
 
-function registrarUsuario() {
+// Evento submit del formulario
+// form.addEventListener('click', registrarUsuario());
 
-    // Creamos el obj de la clase
-    const user = new Usuario();
+function registrarUsuario() {
+    // alert("hola");
 
     // Valores de los campos de texto
     let ux = document.getElementById("ux").value;
     let email = document.getElementById("email").value;
+    let saldo = document.getElementById("saldo").value;
     let pwd = document.getElementById("pwd").value;
+
+    /*console.log(objLocalStorage.id);
+    console.log(objLocalStorage.usuario);
+    console.log(objLocalStorage.email);
+    console.log(objLocalStorage.saldo);
+    console.log(objLocalStorage.password);*/
+
+    // Creamos el obj de la clase
+    const user = new Usuario(cont++, ux, email, saldo, pwd);
+
+    localStorage.setItem(cont++, conversor(user));
 
     /*alert(ux);
     alert(email);
     alert(pwd);*/
 
-    // Creamos el OBJ para guardarlo en el localStorage
-    let objLocalStorage = {
-        id: cont++,
-        usuario: ux,
-        email: email,
-        password: pwd
-    };
-
-    user.guardarLogIn(objLocalStorage);
+    user.guardarLogIn(cont, ux, email, saldo, pwd);
 }
 
 class Usuario {
 
-    constructor(usuario, email, pwd) {
+    constructor(conta, usuario, email, saldo, pwd) {
+        this.contador = conta;
         this.usuario = usuario;
         this.email = email;
+        this.saldo = saldo;
         this.pwd = pwd;
+
+        /*alert('Usuario: ' + this.usuario);
+        alert('Email: ' + this.email);
+        alert('Saldo: ' + this.saldo);
+        alert('Pass: ' + this.pwd);*/
     }
 
-    guardarLogIn(obj) {
-        alert(obj);
+    guardarLogIn(c, u, e, s, p) {
+
+        /*alert('UX: ' + u);
+        alert('Mail: ' + e);
+        alert('Monto: ' + s);
+        alert('PW: ' + p);*/
+
+        // Creamos el OBJ para guardarlo en el localStorage
+        let objLocalStorage = {
+            id: c,
+            usuario: u,
+            email: e,
+            saldo: s,
+            password: p
+        };
+
+        // alert(objLocalStorage.usuario);
+
+
+        // Definimos la variable del array para almacenar el objeto itemObject
+        var arrayLocal;
+
+        if (localStorage.getItem('nuevoUsuario') === null) {
+            // Defino el array
+            arrayLocal = [];
+        } else {
+            arrayLocal = JSON.parse(localStorage.getItem('nuevoUsuario'));
+        }
+
+        // 
+        arrayLocal.push(objLocalStorage);
+
+        // Meterlo en localStorage
+        localStorage.setItem('nuevoUsuario', JSON.stringify(arrayLocal));
+
     }
 
 }
