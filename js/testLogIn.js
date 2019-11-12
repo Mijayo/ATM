4f6578c72d3db4c4812a696e3447b375feba6adb
Diego <48282657+Mijayo@users.noreply.github.com>
2019-11-12 17:54:08 +0100

    testLogIn_V4

--- js/testLogIn.js
+++ js/testLogIn.js
@@ -2,14 +2,77 @@
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+};
+
+let cont = 0;
+
+function registrarUsuario() {
+
+    // Creamos el obj de la clase
+    const user = new Usuario();
+
+    // Valores de los campos de texto
+    let ux = document.getElementById("ux").value;
+    let email = document.getElementById("email").value;
+    let pwd = document.getElementById("pwd").value;
+
+    /*alert(ux);
+    alert(email);
+    alert(pwd);*/
+
+    // Creamos el OBJ para guardarlo en el localStorage
+    let objLocalStorage = {
+        id: cont++,
+        usuario: ux,
+        email: email,
+        password: pwd
+    };
+
+    user.guardarLogIn(objLocalStorage);
+}
+
+class Usuario {
+
+    constructor(usuario, email, pwd) {
+        this.usuario = usuario;
+        this.email = email;
+        this.pwd = pwd;
+    }
+
+    guardarLogIn(obj) {
+        alert(obj);
+    }
+
 }
 
 
-class LogIn {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class LogIn {
 
 
     guardarLogIn() {
@@ -42,7 +105,7 @@ class LogIn {
 
         } else {
             this.guardarLogIn(objLocalStorage);
-        }*/
+        }
 
         // Definimos la variable del array para almacenar el objeto itemObject
         //var arrayLocal;
@@ -68,9 +131,9 @@ class LogIn {
 
         // return cadena;
     }
-}
+}*/
 
-document.getElementById("boton").addEventListener("click",  LogIn.guardarLogIn());
+// document.getElementById("boton").addEventListener("click", LogIn.guardarLogIn());
 // const obj = new LogIn();
 // console.log(obj.guardarLogIn());
 // console.log(email);
